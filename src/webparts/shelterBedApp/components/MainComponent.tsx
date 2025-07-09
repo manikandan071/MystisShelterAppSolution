865be0b89a9a92b8191c409ac493557cb66dcec6
MANIKANDAN <manidhiyamech07167@gmail.com>
2025-07-09 14:27:14 +0530

    initial setup 09/07/25

---
+++ src/webparts/shelterBedApp/components/MainComponent.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import "./style.css";
+
+const MainComponent: React.FC = () => {
+  return <div>MainComponent</div>;
+};
+
+export default MainComponent;
---
+++ src/webparts/shelterBedApp/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IShelterBedAppWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'ShelterBedAppWebPartStrings' {
+  const strings: IShelterBedAppWebPartStrings;
+  export = strings;
+}
